cbe6fe6004023aa7b9b273af1fe837414fc6c11b
Deep Biswas <deepbiswas45510@gmail.com>
2026-03-17 21:28:50 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,25 +1,7 @@
-import subprocess
-import webbrowser
-import time
+from flask import Flask
 
-print("Starting Sentinel AI...")
+app = Flask(__name__)
 
-# start backend
-backend = subprocess.Popen(
-    ["python", "backend/server.py"]
-)
-
-# start frontend
-frontend = subprocess.Popen(
-    ["cmd", "/c", "npm run dev"],
-    cwd="web-app"
-)
-
-# wait for servers to start
-time.sleep(5)
-
-# open browser
-webbrowser.open("http://localhost:5173")
-
-backend.wait()
-frontend.wait()
+@app.route("/")
+def home():
+    return "Backend Running ✅"
